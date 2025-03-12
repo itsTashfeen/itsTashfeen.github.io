5fc4cdef56702a243632b461011ce430a0b5b1aa
Tashfeen Omranm <replytashfeen@gmail.com>
2025-03-11 23:53:54 -0400

    s

---
+++ theme-init.js
@@ -0,0 +1,8 @@
+// theme-init.js
+(function() {
+  // Check for saved theme preference and apply it immediately
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme === 'dark') {
+    document.documentElement.classList.add('dark-theme');
+  }
+})();
